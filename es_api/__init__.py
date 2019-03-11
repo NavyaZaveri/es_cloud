69b611041d89d72dc405db203b121f81ff64bc16
NavyaZaveri <navyajayesh+1@gmail.com>
2019-03-11 02:37:28 +0000

    slight import tweak to make local testing completely independent of production configs

--- es_api/__init__.py
+++ es_api/__init__.py
@@ -1,19 +1,18 @@
 from flask import Flask
-
 from . import blueprint_builder
-from config import ProductionConfig
-from config import TestingConfig
 
 
 def create_app(config_name):
     app = Flask(__name__)
     if config_name == "TESTING":
+        from config import TestingConfig
         app.config.from_object("config.TestingConfig")
         bp = blueprint_builder.create_blueprint(TestingConfig)
-    else:
+    elif config_name == "PRODUCTION":
+        from config import ProductionConfig
         app.config.from_object("config.ProductionConfig")
         bp = blueprint_builder.create_blueprint(ProductionConfig)
-    if bp is None:
+    else:
         raise ValueError("config name can be either TESTING or PRODUCTION")
     app.register_blueprint(bp)
 
